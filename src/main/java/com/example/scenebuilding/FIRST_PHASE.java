dedfec2645f27b6b686101468699e381a31435b1
ronnks <ronnskronald99@gmail.com>
2022-07-04 05:47:10 -0400

    Initial commit

---
+++ src/main/java/com/example/scenebuilding/FIRST_PHASE.java
@@ -0,0 +1,17 @@
+package com.example.scenebuilding;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+    public class FIRST_PHASE {
+
+
+}
